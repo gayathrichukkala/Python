8f43d93070d05b7bc16069b9e41d3bb949b59fb9
gayathrichukkala <gayathrikrishna1899@gmail.com>
2024-07-15 16:43:45 +0530

    updated

---
+++ app.py
@@ -0,0 +1,15 @@
+x=2
+y=3.0
+z="sample"
+a=[1,2,3,4.0,"length"]
+print(x)
+print(y)
+print(z)
+print(a)
+b={"a":8,"b":9,"c":"length"}
+print(type(a))
+print(a[3])
+print(b['a'])
+s=x+y
+print(s)
+print(x+y)
