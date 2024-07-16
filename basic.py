122d9c473632a2dfa376e19b104f17a8cee351ca
gayathrichukkala <gayathrikrishna1899@gmail.com>
2024-07-16 17:48:45 +0530

    Exercises

---
+++ basic.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number:"))
+b=float(input("enter the second number:"))
+c=a+b
+print(c)
+d=a*b
+print(d)
---
+++ totalmarks.py
@@ -0,0 +1,12 @@
+x=int(input("enter the frist number:"))
+y=int(input("enter the second number:"))
+a=int(input("enter the third number:"))
+# b=int(input("enter the fourth number"))
+# c=int(input("enter the fifth number:"))
+print(x)
+total=x/100
+print(total)
+
+sum=x+y+a
+avg=(sum)/3
+print(sum,avg)
