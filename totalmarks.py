f8204eecbb2fbf45dcf3ec23c95f215eb4723c22
gayathrichukkala <gayathrikrishna1899@gmail.com>
2024-07-16 17:55:13 +0530

    some changes

--- totalmarks.py
+++ totalmarks.py
@@ -1,8 +1,8 @@
 x=int(input("enter the frist number:"))
 y=int(input("enter the second number:"))
 a=int(input("enter the third number:"))
-# b=int(input("enter the fourth number"))
-# c=int(input("enter the fifth number:"))
+b=int(input("enter the fourth number"))
+c=int(input("enter the fifth number:"))
 print(x)
 total=x/100
 print(total)
